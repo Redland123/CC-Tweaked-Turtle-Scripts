d144afb27c8ed0d5361d7840bbfb38311687c635
Bradley Odell <btodell@hotmail.com>
2019-12-23 10:38:27 -0800

    Testing `craftbat` script...

--- src/craftbat.lua
+++ src/craftbat.lua
@@ -1,19 +1,45 @@
-function extractItem(quantity, itemID, slots, outOfMessage, incorrectMessage)
-
+function extractItem(quantity, itemID, subID, slots, outOfMessage, incorrectMessage)
+    for _, slot in ipairs(slots) do
+        -- Suck item from chest into slot
+        turtle.select(slot)
+        local suckResult = turtle.suck(quantity)
+        if not suckResult then
+            print("Error: " .. outOfMessage)
+            return false
+        end
+        -- Verify item in slot
+        local itemData = turtle.getItemDetail()
+        if not itemData then
+            print("Error?: " .. outOfMessage)
+            return false
+        end
+        if (itemData.name ~= itemID) or ((subID ~= nil) and (itemData.damage ~= subID)) then
+            print("Error: " .. incorrectMessage)
+            return false
+        end
+        if itemData.count ~= quantity then
+            print("Error: Incorrect number of items in slot " .. slot)
+            return false
+        end
+    end
+    return true
 end
 
 function nextChest()
 
+    return false
 end
 
 function returnToFirstChest()
 
+    return false
 end
 
 function craft()
+    local quantity = 1
     -- Assumes turtle is in front of left-most chest
     -- Take 1 insulated copper cable
-    if not extractItem(1, "ic2:cable", {2}, "Out of insulated copper cable!", "No insulated copper cable in first chest!") then
+    if not extractItem(quantity, "ic2:cable", 0, {2}, "Out of insulated copper cable!", "No insulated copper cable in first chest!") then
         return false
     end
     -- Move to redstone chest
@@ -22,7 +48,7 @@ function craft()
         return false
     end
     -- Take 2 redstone
-    if not extractItem(1, "", {6, 10}, "Out of redstone!", "No redstone in second chest!") then
+    if not extractItem(quantity, "minecraft:redstone", nil, {6, 10}, "Out of redstone!", "No redstone in second chest!") then
         return false
     end
     -- Move to tin ingot nextChest
@@ -31,9 +57,16 @@ function craft()
         return false
     end
     -- Take 4 tin ingots
-    if not extractItem(1, "", {}, "Out of tin ingots!", "No tin ingots in last chest!") then
+    if not extractItem(quantity, "thermalfoundation:material", 129, {5, 7, 9, 11}, "Out of tin ingots!", "No tin ingots in last chest!") then
         return false
     end
+    -- Move to deposit chest
+    if not nextChest() then
+        print("Error: Failed to move to deposit chest!")
+        return false
+    end
+    -- Deposit crafted item
+    -- TODO
     -- Return to first chest
     if not returnToFirstChest() then
         print("Error: Failed to return to insulated copper chest!")
@@ -49,4 +82,5 @@ if (not success) or (data.name ~= "minecraft:chest") then
     return
 end
 
-while craft() do end
+craft()
+--while craft() do end
