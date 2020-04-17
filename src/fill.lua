3f74a0cba0eacc4257179f8d1158c734696be55d
Bradley Odell <btodell@hotmail.com>
2020-04-17 01:19:37 -0700

    Fixed some bugs in fill script.

--- src/fill.lua
+++ src/fill.lua
@@ -1,19 +1,18 @@
---Gets user input and saves it to a variable that makes sense
+-- Gets user input and saves it to a variable that makes sense
 local targs = { ... }
 local diameter = targs[1]
 
---"Constants"
+-- Constants
 local invSize = 16
 
---funcitons
---Finds and selects the first block in the inventory
+-- Finds and selects the first block in the inventory
 function findBlock()
     for i = 1, invSize do
         if turtle.getItemDetail(i) then
-            if turtle.getSelectedSlot ~= i then
+            if turtle.getSelectedSlot() ~= i then
                 turtle.select(i)
-                return true
             end
+            return true
         end
     end
     return false
@@ -21,23 +20,23 @@ end
 
 function placeLine()
     for i = 1, diameter do
-        --Find the first block in the inventory
-        if findBlock() ~= true then
-            print("Error, no blocks found. ")
+        -- Find the first block in the inventory
+        if not findBlock() then
+            print("Error: No blocks found.")
 
-            --loops findblock till a block is provided
+            -- Waits until block is placed in inventory
             while not findBlock() do
                 sleep(1)
             end
         end
 
-        --Attemps to place the currently selected block under the turtle
+        -- Attemps to place the currently selected block under the turtle
         turtle.forward()
         turtle.placeDown()
     end
 end
 
---Moves the turtle into the correct location for creating a new line
+-- Moves the turtle into the correct location for creating a new line
 function moveNext(side)
     if side == 0 then
         turtle.turnRight()
@@ -53,14 +52,17 @@ end
 function main()
     local side = 0
 
-        for i = 1, diameter do
-            placeLine()
+    for i = 1, diameter do
+        placeLine()
 
-            if side == 0 then side = 1
-            else side = 0 end
+        if side == 0 then
+            side = 1
+        else 
+            side = 0
+        end
 
-            moveNext(side)
-        end             
+        moveNext(side)
+    end
 end
 
 --Runs the main function
