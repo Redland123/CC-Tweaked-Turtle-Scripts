ad045bd1202b3eeab14170fd2b6ea390902da2a2
Redland123 <David.50mg@gmail.com>
2023-06-08 22:39:28 -0700

    Install script fix

--- install.lua
+++ install.lua
@@ -23,7 +23,7 @@ local libs = {
 
 local args = { ... }
 local download = args[1] == "download"
-local urlPrefix = "https://raw.githubusercontent.com/Redland123/computercraft-turtle-scripts/master"
+local urlPrefix = "https://raw.githubusercontent.com/Redland123/computercraft-turtle-scripts/master/"
 
 function downloadLua(url, path)
     if not path then
